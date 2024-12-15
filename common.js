a60ebce246249a2e8ed80719fb88ecd079e974e4
Justin Siebert <19justinsiebert99@gmail.com>
2024-12-15 17:33:48 +0100

    starting with the common css

---
+++ common.js
@@ -0,0 +1,10 @@
+const body = document.querySelector("body")
+const submit = document.getElementById("submit")
+const text = document.getElementById("text")
+const reset = document.getElementById("reset")
+
+submit.addEventListener("click", function (e){
+    // skapa en funktion som startar en funktion som tar nummer i input
+    // och skapar X antal div efter nummer som står med kombination
+    // av mathrandom() som skapar olika siffor mellan 0-100
+})
