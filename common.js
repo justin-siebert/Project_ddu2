1dac3ebd8949dfb7dbb40fdbe3d0f9a39b57440e
Justin Siebert <19justinsiebert99@gmail.com>
2024-12-16 12:56:39 +0100

    gridtable done with random numbers adding upp. New touches for the css used

--- common.js
+++ common.js
@@ -1,10 +1,35 @@
 const body = document.querySelector("body")
 const submit = document.getElementById("submit")
-const text = document.getElementById("text")
 const reset = document.getElementById("reset")
+const grid = document.getElementById("gridtable")
 
+
+function createGrid (numberOfBoxes){
+    grid.innerHTML = "";
+    for (let i = 1; i <= numberOfBoxes; i++){
+        let div = grid.appendChild(document.createElement("div"));
+        let randomNumber = Math.floor(Math.random()*99)+1;
+        div.textContent = randomNumber
+    }
+};
+
+submit.addEventListener("click", ()=> {
+    const text = document.getElementById("text").value
+    let numberOfBoxes = parseInt(text);
+    if (!isNaN(numberOfBoxes) && numberOfBoxes >0){
+        createGrid(numberOfBoxes)
+    }
+});
+
+reset.addEventListener("click", ()=>{
+    grid.innerHTML = ""
+});
+
+/*
 submit.addEventListener("click", function (e){
     // skapa en funktion som startar en funktion som tar nummer i input
     // och skapar X antal div efter nummer som står med kombination
     // av mathrandom() som skapar olika siffor mellan 0-100
+
 })
+*/
