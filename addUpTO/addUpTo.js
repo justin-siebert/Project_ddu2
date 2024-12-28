57fa266ec54036ca156308eb7521169e1d61e24b
Justin Siebert <19justinsiebert99@gmail.com>
2024-12-28 13:57:32 +0100

    finishing touch for addupto site

--- addUpTO/addUpTo.js
+++ addUpTO/addUpTo.js
@@ -1,13 +1,25 @@
 const numberAddUpTo = document.getElementById("numberAddUpTo")
+const addingUpButton = document.getElementById("addingUpButton")
 
 
 function addUp(){
     const allDivs = document.querySelectorAll("#gridtable div")
-    let result = parseInt(numberAddUpTo.textContent)
-    allDivs.forEach((div)=>{
-        if (parseInt(div.textContent) && parseInt(div.textContent) === result) {
-            div.classList.add("marked")
+    let result = parseInt(numberAddUpTo.value)
+    allDivs.forEach((div) => div.classList.remove("marked"));
+
+
+    for (let i = 0; i < allDivs.length; i++){
+        let number1 = parseInt(allDivs[i].textContent)
+        for (let j = i + 1; j < allDivs.length; j++) {
+            const number2 = parseInt(allDivs[j].textContent)
+            if(number1 + number2 == result){
+                allDivs[i].classList.add("marked")
+                allDivs[j].classList.add("marked")
+                return
+            }
         }
-    })
     
+    }
 }
+
+addingUpButton.addEventListener("click", addUp)
