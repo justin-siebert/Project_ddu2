fd4474433c678ea8709211680c1fe07d07234add
Justin Siebert <19justinsiebert99@gmail.com>
2025-01-06 13:27:57 +0100

    changed gamestart function

--- Blackjack/blackjack.js
+++ Blackjack/blackjack.js
@@ -1,4 +1,14 @@
 const body = document.querySelector("body")
+const showMoneyOnScreen = document.getElementById("showMoneyOnScreen")
+const tapToStart = document.getElementById("tapToStart")
+const hitButton = document.getElementById("hitButton")
+const standButton = document.getElementById("standButton")
+const resultMessage = document.getElementById("resultMessage")
+
+let currentPlayerIndex = 0;
+let players = [];
+let deck = [];
+let playerScores = [];
 
 function createDeck() {
     let cardType = ["♥️", "♠️", "♣️", "♦️"];
@@ -14,9 +24,9 @@ function createDeck() {
     return deck;
 }
 
-function shuffleDeck (deck){
-    for (let i = deck.length; i > 0; i--){
-        let j = Math.floor(Math.random() * (i + 1))
+function shuffleDeck(deck) {
+    for (let i = deck.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
         [deck[i], deck[j]] = [deck[j], deck[i]];
     }
 }
@@ -37,10 +47,62 @@ function createPlayers (numberOfPlayers){
 }
 
 function dealCards(players, deck, cardsPerPlayer = 2){
-    for (let i = 0; i < players.length; i++){
+    for (let i = 0; i < cardsPerPlayer; i++){
         for (let player of players){
             const card = deck.pop()
             player.hand.push(card)
         }
     }
 }
+
+function calculateHandValue(hand){
+    let aces = 0
+    let value = 0
+
+    hand.forEach(card => {
+        const cardValue = card.split(" ")[0]
+        if (cardValue=== "Ace"){
+            value += 11;
+            aces++
+        } else if (["Jack", "Queen", "King"].includes(cardValue)){
+            value += 10
+        } else {
+            value += parseInt(cardValue)
+        }    
+    });
+
+    while (value > 21 && aces > 0){
+        value -= 10
+        aces--
+    }
+    return value
+
+}
+
+function gameStart (){
+    let numberOfPlayers = parseInt(prompt("Enter a number of Players (1-3)"))
+    let buyIn = parseInt(prompt("Enter the buy in amount"))
+    showMoneyOnScreen.textContent =`${buyIn}`
+
+    const deck = createDeck();
+    shuffleDeck(deck);
+
+    const players = createPlayers(numberOfPlayers);
+
+    dealCards(players, deck);
+    
+    if (isNaN(numberOfPlayers) || numberOfPlayers <1 || numberOfPlayers >3){
+        alert("Please enter a valid number of Players between 1 and 3")
+        return
+    }
+
+    if (isNaN(buyIn) || buyIn <= 0){
+        alert ("Enter a valid amount")
+        return
+    }
+
+    console.log("Players and their hands:", players);
+    return players;
+}
+
+tapToStart.addEventListener("click", gameStart)
